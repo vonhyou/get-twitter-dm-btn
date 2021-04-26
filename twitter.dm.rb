7ae920f1ff98f42c05b9411b0c343aaaf0a97c93
vonhyou <vonhyou@yeah.net>
2021-04-26 23:45:43 +0800

    add handle to id

--- twitter.dm.rb
+++ twitter.dm.rb
@@ -1,13 +1,23 @@
 require 'sinatra'
+require 'net/http'
+
+def handle_to_id(handle)
+  get_id_uri = URI 'https://tweeterid.com/ajax.php'
+
+  res = Net::HTTP.post_form(get_id_uri, 'input': handle)
+  res.body.to_s
+end
 
 get '/' do
   erb :index
 end
 
 post '/' do
-  unless params[:id].empty? or params[:content].empty?
+  unless params[:handle].empty? or params[:content].empty?
     baselink = "https://twitter.com/messages/compose?text"
-    @permlink = "#{baselink}=#{URI.encode params[:content]}&recipient_id=#{params[:id]}"
+    greetings = URI.encode params[:content]
+    twitter_id = handle_to_id params[:handle]
+    @permlink = "#{baselink}=#{greetings}&recipient_id=#{twitter_id}"
   end
   erb :index
 end
