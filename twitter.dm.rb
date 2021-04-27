081d0e1897ac1d5db27cd837c23a38a950306bc1
vonhyou <vonhyou@yeah.net>
2021-04-27 10:06:43 +0800

    remake

--- twitter.dm.rb
+++ twitter.dm.rb
@@ -8,16 +8,20 @@ def handle_to_id(handle)
   res.body.to_s
 end
 
+def make_permlink(content, handle)
+  baselink = "https://twitter.com/messages/compose?text"
+  greetings = URI.encode content
+  twitter_id = handle_to_id handle
+  "#{baselink}=#{greetings}&recipient_id=#{twitter_id}"
+end
+
 get '/' do
   erb :index
 end
 
 post '/' do
   unless params[:handle].empty? or params[:content].empty?
-    baselink = "https://twitter.com/messages/compose?text"
-    greetings = URI.encode params[:content]
-    twitter_id = handle_to_id params[:handle]
-    @permlink = "#{baselink}=#{greetings}&recipient_id=#{twitter_id}"
+    @permlink = make_permlink params[:content], params[:handle]
   end
   erb :index
 end
